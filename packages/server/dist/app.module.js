630ea8874e333326de6e2db21c88cc019e0828b3
yxuanzhang <yxuanzhang@tencent.com>
2023-05-17 23:09:06 +0800

    耗时任务并发测试

--- packages/server/dist/app.module.js
+++ packages/server/dist/app.module.js
@@ -24,15 +24,21 @@ AppModule = __decorate([
                 isGlobal: true,
                 envFilePath,
             }),
-            typeorm_1.TypeOrmModule.forRoot({
-                type: 'mysql',
-                host: '9.134.243.217',
-                port: 3306,
-                username: 'root',
-                password: '123',
-                database: 'blowsysun',
-                entities: [__dirname + '/**/*.entity{.ts,.js}'],
-                synchronize: true,
+            typeorm_1.TypeOrmModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                inject: [config_1.ConfigService],
+                useFactory: (config) => {
+                    return {
+                        type: 'mysql',
+                        host: config.get('DATABASE_HOST'),
+                        port: config.get('DATABASE_PORT'),
+                        username: config.get('DATABASE_USER'),
+                        password: config.get('DATABASE_PASSWORD'),
+                        database: config.get('DATABASE_DATABASE'),
+                        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                        synchronize: true,
+                    };
+                },
             }),
             pay_back_module_1.PayBackModule,
             schedule_1.ScheduleModule.forRoot()
--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -15,34 +15,34 @@ const envFilePath = `.env.${process.env.NODE_ENV || 'prod'}`;
       envFilePath,
     }),
     //配置数据库链接
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (config: ConfigService) => {
-    //     return {
-    //       type: 'mysql',
-    //       host: config.get('DATABASE_HOST'),
-    //       port: config.get('DATABASE_PORT'),
-    //       username: config.get('DATABASE_USER'),
-    //       password: config.get('DATABASE_PASSWORD'),
-    //       database: config.get('DATABASE_DATABASE'),
-    //       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //       synchronize: true,
-    //     } as TypeOrmModuleOptions;
-    //   },
-    // }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      // host: '43.154.209.141',
-      // devCloud
-      host: '9.134.243.217',
-      port: 3306,
-      username: 'root',
-      password: '123',
-      database: 'blowsysun',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: config.get('DATABASE_HOST'),
+          port: config.get('DATABASE_PORT'),
+          username: config.get('DATABASE_USER'),
+          password: config.get('DATABASE_PASSWORD'),
+          database: config.get('DATABASE_DATABASE'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        } as TypeOrmModuleOptions;
+      },
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   // host: '43.154.209.141',
+    //   // devCloud
+    //   host: '9.134.243.217',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '123',
+    //   database: 'blowsysun',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
     PayBackModule,
     ScheduleModule.forRoot()
   ],
