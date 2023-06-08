fd7c9497469e035ed5de7844dab49f1b421ffa9e
yxuanzhang <yxuanzhang@tencent.com>
2023-06-08 18:46:53 +0800

    界面调优

--- packages/client/dist/assets/detail.877bc00f.js
+++
@@ -1,1 +0,0 @@
-import{d as t,o,l as n}from"./index.e52ed64e.js";const c={class:"container"},s=t({name:"newsDetail"}),l=t({...s,setup(r){function e(){JSON.parse(localStorage.getItem("formData")||"{}").content}return e(),(a,_)=>(o(),n("div",c))}});export{l as default};
