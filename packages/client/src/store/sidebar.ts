fd74dd26629684bf6674af18b1e4dc8bbad0d609
yxuanzhang <yxuanzhang@tencent.com>
2023-08-04 18:42:36 +0800

    调整界面自动刷新时间为交易时间

--- packages/client/src/store/sidebar.ts
+++ packages/client/src/store/sidebar.ts
@@ -1,11 +1,22 @@
 import { defineStore } from 'pinia';
+import dayjs from 'dayjs';
+
+// isAutoRefresh 根据时间判断初始值是否为true；交易时间为true
+const currentTime = dayjs();
+const currentDate = currentTime.format('YYYY-MM-DD');
+
+function isTradeTime() {
+  const day = currentTime.day();
+  // 周一到周五 9.19 - 15:00 为交易时间；
+  return day >= 1 && day <= 5 && currentTime.isAfter(currentDate + ' 09:19:00') && currentTime.isBefore(currentDate + ' 15:00:00');
+}
 
 export const useSidebarStore = defineStore('sidebar', {
   state: () => {
     return {
       collapse: true,
       countDays: 15,
-      isAutoRefresh: true,
+      isAutoRefresh: isTradeTime(),
     };
   },
   getters: {},
