10b6a9887ac79801c202c3fbe7190467da065a08
yxuanzhang <yxuanzhang@tencent.com>
2023-08-20 21:39:52 +0800

    core包提交

--- packages/server/src/filters/HttpExceptionFilter.ts
+++ packages/server/src/filters/HttpExceptionFilter.ts
@@ -9,7 +9,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
-
+    
     response
       .status(status)
       .json({
