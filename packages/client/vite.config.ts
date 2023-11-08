22b80b2fd15094f74e60723b15657f65ac6a7387
yxuanzhang <yxuanzhang@tencent.com>
2023-11-08 23:18:32 +0800

    update

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -91,7 +91,7 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
 							return 'vendor-utils';
 						}
 						// 剩余的外部依赖全部装入 utils中
-						if (id.indexOf('/node_modules') !== -1) {
+						if (id.indexOf('/node_modules/') !== -1) {
 							return 'vendor-external';
 						}
 					},
