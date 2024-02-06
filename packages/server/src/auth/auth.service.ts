7f44b88b681f74607cef7b309a755262a4d7907f
yxuanzhang <yxuanzhang@tencent.com>
2024-02-06 17:25:06 +0800

    update

--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDTO } from './dto/login.dto';
-import * as session from 'express-session';
 
 @Injectable()
 export class AuthService {
